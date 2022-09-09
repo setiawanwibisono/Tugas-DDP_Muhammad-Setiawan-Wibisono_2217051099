571f2fbc3645375d490fd14ba33563c182c6fcaf
setiawanwibisono <112566057+setiawanwibisono@users.noreply.github.com>
2022-09-09 19:18:32 +0700

    Add files via upload

---
+++ 2217051099_Muhammad Setiawan Wibisono_Latihan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float luas,r,pi = 3.14 ;
+	cout<<"menghitung luas lingkaran\n";
+	cout<<"masukan jari jari \n";
+	cin>>r;
+	luas=pi*r*r ;
+	cout<<"luas lingkaran adalah : "<<luas;
+	return 0;
+}
